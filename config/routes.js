b11456874fd485b7b65b76b505f3fa351b714961
AldiAgeng <aldiageng48@gmail.com>
2022-06-22 15:12:05 +0700

    add authUtils (encryptPassowrd,checkPassowrd,createToken), change file routes, add env

--- config/routes.js
+++ config/routes.js
@@ -7,11 +7,6 @@ const apiRouter = express.Router();
  * TODO: Implement your own API
  *       implementations
  */
-apiRouter.get("/api/v1/posts", controllers.api.v1.postController.list);
-apiRouter.post("/api/v1/posts", controllers.api.v1.postController.create);
-apiRouter.put("/api/v1/posts/:id", controllers.api.v1.postController.update);
-apiRouter.get("/api/v1/posts/:id", controllers.api.v1.postController.show);
-apiRouter.delete("/api/v1/posts/:id", controllers.api.v1.postController.destroy);
 
 /**
  * TODO: Delete this, this is just a demonstration of
